7faea1baba95b7420f4300bb6151eaa1523972e9
Samuel <sagg160800@gmail.com>
2024-03-13 12:16:04 -0400

    user_routes_updated

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require('express')
 const {post, getAll, getById, update, remove} = require('../controllers/user.controllers')
-const { verifyToken, isAdmin, isModerator } = require('../middlewares')
+const { verifyToken, isAdmin, isAuthor} = require('../middlewares')
 const router = Router()
 
 module.exports = router
@@ -9,8 +9,8 @@ router.post("/", [verifyToken, isAdmin], post)
 
 router.get("/", [verifyToken, isAdmin], getAll)
 
-router.get("/:id", [verifyToken, isAdmin], getById)
+router.get("/:id", [verifyToken, isAuthor, isAdmin], getById)
 
-router.put("/:id", [verifyToken, isAdmin], update)
+router.put("/:id", [verifyToken, isAuthor, isAdmin], update)
 
-router.delete("/:id", [verifyToken, isAdmin], remove)
+router.delete("/:id", [verifyToken, isAuthor, isAdmin], remove)
